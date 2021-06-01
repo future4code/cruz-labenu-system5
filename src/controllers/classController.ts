13a30f8499be882d936aa0ff986a7ec7eafd6933
Fagner Zulin <fagnerzulin11@gmail.com>
2021-06-01 17:18:00 -0300

    Done create-class

--- src/controllers/classController.ts
+++ src/controllers/classController.ts
@@ -1,7 +1,23 @@
 import { Response, Request } from "express";
+import { labenuClass } from "../types/class";
+import { v4 as uuidv4 } from "uuid";
+import { createClass } from "../data/classQuerys";
 
 export default class ClassController {
   postClass = async (req: Request, res: Response) => {
+    const { name, startDate, endDate, shift, module } = req.body;
+
+    const data: labenuClass = {
+      name,
+      startDate,
+      endDate,
+      id: uuidv4(),
+      shift,
+      module,
+    };
+
+    await createClass(data);
+    res.send({ message: "Created class" });
     try {
     } catch (error) {
       res.status(400).send(error.message);
